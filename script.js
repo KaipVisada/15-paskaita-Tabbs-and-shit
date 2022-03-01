1204bb91e679354cc416a39acbf5bef8f6398350
DDubrovinas <kaipvisada@gmail.com>
2022-03-01 16:15:25 +0200

    Tabs and stuff

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict"
+    function changeHeader(event, name){
+        var tabs = document.querySelectorAll(".tab");
+        var buttons =document.querySelectorAll("button");
+        var sound = document.querySelector("#sound");
+        sound.play();
+
+        setTimeout(function(){
+            sound.pause();
+            
+        }, 3000);
+
+        for(var i = 0; i < tabs.length; i++){
+            tabs[i].style.display = "none";
+            buttons[i].classList.remove("selected")
+        }
+
+
+
+        document.getElementById(name).style.display = "block";
+        event.currentTarget.classList.add("selected")
+
+
+        setTimeout(function(){
+            var tabs = document.querySelectorAll(".tab");
+            for(var x of tabs){
+                x.classList.remove("tabAnimation");
+            }
+
+            document.getElementById(name).classList.add
+            ("tabAnimation");
+        }, 10);
+}       
+changeHeader("Pirmas");
+
+      function darkMode() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+      }
